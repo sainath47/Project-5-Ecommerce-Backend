fa256921331272a877955938c65a59ac00349a8c
pareshnaik96 <pareshnaik678@gmail.com>
2022-05-31 17:21:39 +0530

    review done

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,36 +1,46 @@
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
+
+
 const isValidObjectId = function (ObjectId) {
    return mongoose.Types.ObjectId.isValid(ObjectId)
 }
-const authentication =async function(req,res,next){
 
-let token = req.headers["authorization"]
-if(!token)return res.status(401).send({ status: false, msg : "please provide token " })
 
-let decodedToken=jwt.verify(token.split(" ")[1],"functionUp")
-if(decodedToken) next()
-else return res.status(401).send({ status: false, msg : "authentication failed,invalid token " })
+const authentication = async function (req, res, next) {
+
+   let token = req.headers["authorization"]
+   if (!token) return res.status(401).send({ status: false, msg: "please provide token " })
+
+   let decodedToken = jwt.verify(token.split(" ")[1], "functionUp")
+   if (decodedToken)
+   next()
+   else return res.status(401).send({ status: false, msg: "authentication failed,invalid token " })
 
 }
 
-const authorization =async function(req,res,next){
 
-let token = req.headers["authorization"]
-if(!token)return res.status(401).send({ status: false, msg : "please provide token " })
 
-let decodedToken=jwt.verify(token.split(" ")[1],"functionUp")// BearerToken token 
-let  userId = req.params.userId
-if(!isValidObjectId(userId))return  res.status(400).send({status:false,message:"invalid userId"})
-if(decodedToken){ 
-   if(decodedToken.userId== userId)  next()
-    else return res.status(403).send({ status: false, msg : "unauthorized" })
+const authorization = async function (req, res, next) {
+
+   let token = req.headers["authorization"]
+   if (!token) return res.status(401).send({ status: false, msg: "please provide token " })
+
+   let decodedToken = jwt.verify(token.split(" ")[1], "functionUp")      //----- BearerToken token
+
+   let userId = req.params.userId
+   if (!isValidObjectId(userId)) return res.status(400).send({ status: false, msg: "invalid userId" })
+
+   if (decodedToken) {
+      if (decodedToken.userId == userId) 
+      next()
+      else return res.status(403).send({ status: false, msg: "unauthorized" })
    }
-else return res.status(401).send({ status: false, msg : "authentication failed,invalid token" })
+   else return res.status(401).send({ status: false, msg: "authentication failed,invalid token" })
 
 }
 
 
 
 
-module.exports = {authentication,authorization}
+module.exports = { authentication, authorization }
